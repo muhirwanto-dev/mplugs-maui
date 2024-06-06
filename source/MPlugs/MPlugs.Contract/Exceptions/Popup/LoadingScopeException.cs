527ad5068dfc960dd0d9605746b6b37b46d98ad2
muhirwanto-dev <muhammadirwanto.dev@gmail.com>
2024-06-06 16:42:10 +0800

    init: initial commit to add file

---
+++ source/MPlugs/MPlugs.Contract/Exceptions/Popup/LoadingScopeException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPlugs.Contract.Exceptions.Popup
+{
+    public class LoadingScopeException : Exception
+    {
+        public enum EExceptionType
+        {
+            ScopeAlreadyExist,
+            ScopeNotMatched
+        }
+
+        public LoadingScopeException(EExceptionType type, string? message = null)
+            : base(message)
+        {
+        }
+    }
+}
---
+++ source/MPlugs/MPlugs.Popup/PageLoading.cs
@@ -0,0 +1,19 @@
+﻿using MPlugs.Contract.Exceptions.Popup;
+
+namespace MPlugs.Popup
+{
+    public partial class PageLoading
+    {
+        private string? _scope = null;
+
+        public void Show(string message, string scope = "")
+        {
+            if (_scope != null)
+            {
+                throw new LoadingScopeException(LoadingScopeException.EExceptionType.ScopeAlreadyExist);
+            }
+
+            _scope = scope;
+        }
+    }
+}
